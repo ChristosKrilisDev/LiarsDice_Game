eaac2ffacc684686ffeedd69085dc775370625c1
ChristosKrilisDev <christos.krilis.dev@gmail.com>
2020-09-26 18:15:33 +0300

    Upload DieController

---
+++ LIARSDICESGAME/Assets/Scripts/DieController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieController : MonoBehaviour
+{
+
+
+    private void Start()
+    {
+        RotateToFace(1);
+    }
+
+    public void HideDie()
+    {
+        gameObject.SetActive(false);
+    }
+
+
+    public void RotateToFace(int faceNumber)
+    {
+        if(faceNumber > 6 || faceNumber < 1)
+            Debug.LogError("You piece of sh*t gave number out of range");
+
+        transform.rotation = Quaternion.identity;
+        
+
+
+        switch(faceNumber)
+        {
+
+            case 1:
+            {
+                transform.localRotation = Quaternion.Euler(-90,0,0);
+                break;
+            }
+            case 2:
+            {
+                transform.localRotation = Quaternion.Euler(0 , 0 , -90);
+                break;
+            }
+
+            case 3:
+            {
+                transform.localRotation = Quaternion.Euler(0 , 0 , 0);
+                break;
+            }
+
+            case 4:
+            {
+                transform.localRotation = Quaternion.Euler(-180 , 0 , 0);
+                break;
+            }
+
+            case 5:
+            {
+                transform.localRotation = Quaternion.Euler(0 , 0 , 90);
+                break;
+            }
+
+            case 6:
+            {
+                transform.localRotation = Quaternion.Euler(90 , 0 , 0);
+                break;
+            }
+
+        }
+    }
+
+}
--- LIARSDICESGAME/Assets/Scripts/PlayerController.cs
+++ LIARSDICESGAME/Assets/Scripts/PlayerController.cs
@@ -21,8 +21,8 @@ public class PlayerController : MonoBehaviour
 
 
     private int _nDiceRange = 5;    //The total numbers of dices the player have
-    private List<int> _pDices;      //The list of the dices that the player roll(values)
-
+    private List<int> _pDice;      //The list of the dices that the player roll(values)
+    [SerializeField] private List<DieController> _diceObjs;
 
     [SerializeField] private int _gold = 100;       //Later Use : Control the max rounds (-10g / round)
     private int _currentBet = 10;       // gold cost / round
@@ -44,8 +44,11 @@ public class PlayerController : MonoBehaviour
         //Add listener to buttons via script to avoid our dump stupid brain to set them manualy...
         _playerHUD._readyBtn.onClick.AddListener(SetReadyAction);
         _playerHUD._goldeBtn.onClick.AddListener(BetGoldActionOnClick);
-        _playerHUD._diceRollBtn.onClick.AddListener(RollDicesAction);
+        _playerHUD._diceRollBtn.onClick.AddListener(RollDiceAction);
 
+
+        _gold = 100;
+        _currentBet = 10;
 
         //Initialize Player 
         Init();
@@ -59,10 +62,10 @@ public class PlayerController : MonoBehaviour
     //TEST: usage = for test perpose is used as reset fuction as well
     void Init()
     {
-        _gold = 100;
+        //_gold = 100;
+        //_currentBet = 10;
         _playerHUD.UpdateGoldText(_gold);
 
-        _currentBet = 10;
 
         _isReady = false;
         _hasBet = false;
@@ -97,12 +100,19 @@ public class PlayerController : MonoBehaviour
         _playerHUD.UpdateReadyActionParemeters();
     }
 
-    public void RollDicesAction()
+    public void RollDiceAction()
     {
         //Calculate and copy the RNG dices List from the ROundController
-        _pDices = _roundController.CreateRNGDiceList(_nDiceRange);
+        _pDice = _roundController.CreateRNGDiceList(_nDiceRange);
 
-        _playerHUD.DisplayDicesUI(_pDices);
+        _playerHUD.DisplayDicesUI(_pDice);
+
+        for(int i = 0; i < _diceObjs.Count; i++)
+        {
+            //Rotate Dice
+            _diceObjs[i].RotateToFace(_pDice[i]);
+        }
+
 
         //RESET USE
         Init();
@@ -118,6 +128,12 @@ public class PlayerController : MonoBehaviour
     private void CallAction()
     {
         Debug.Log("Call action has done");
+    }
+
+    private void LoseDie()
+    {
+        _diceObjs[0].HideDie();
+        _diceObjs.RemoveAt(0);
     }
     #endregion
 
